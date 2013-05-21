56a30f411b5992f4349b7aeec63521089f97f82c
Charlie <charlie.hoey@gmail.com>
2013-05-20 22:00:59 -0400

    basic demo of spinning bricks

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ require.config({
 		"vendor/Box2dWeb-2.1.a.3": {
 			exports: "Box2D"
 		},
-		"vendor/pixi.dev.js": {
+		"vendor/pixi.dev": {
 			exports: "PIXI"
 		}
 	}
@@ -18,29 +18,72 @@ require.config({
 require(
 	[
 		'vendor/jquery-1.9.1.min',
+		'vendor/pixi.dev',
 		'bin/DOMtra'
 	],
-	function($, DOMtra) {
+	function($, PIXI, DOMtra) {
 
 		$(document).ready(function() {
 
 			// create a new DOMtra instance
 			var m_DOMtra = new DOMtra();
 
-			// create a bunch of platforms
-			for(var i = 0; i < 3; i++) {
-				a = m_DOMtra.createActor('PlatformActor', { x: (window.innerWidth / 2) * Math.random() + (window.innerWidth / 4) - 64, y: i * 250 + 100 })
+
+
+
+			// create an new instance of a pixi stage
+			var stage = new PIXI.Stage(0x66FF99);
+			
+			// create a renderer instance
+			var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, null);
+			
+			// add the renderer view element to the DOM
+			document.body.appendChild(renderer.view);
+			
+			requestAnimFrame( animate );
+			
+			// create a texture from an image path
+			var texture = PIXI.Texture.fromImage("images/brick.gif");
+			// create a new Sprite using the texture
+
+			var bunnies = []
+
+			var bunnyCount = 10000;
+
+			for(var i = 0; i < bunnyCount; i++) {
+
+
+
+				bunnies.push(new PIXI.Sprite(texture));
+			
+				var bunny = bunnies[bunnies.length - 1]
+
+				// center the sprites anchor point
+				bunny.anchor.x = 0.5;
+				bunny.anchor.y = 0.5;
+				
+				// move the sprite t the center of the screen
+				bunny.position.x = Math.random() * window.innerWidth;
+				bunny.position.y = Math.random() * window.innerHeight;
+				bunny.rotationSpeed = Math.random() * .4;
+				
+				stage.addChild(bunny);
 			}
 
-			// make a little particle system that spawns new boxes
-			m_DOMtra.customUpdate = function() {
-				var maxParticles = 50;
-				if(this.actorManager.actors.length < maxParticles) {
-					this.createActor('EnemyTurtleActor', { x: Math.random() * document.width, y: -50 })
-				}
+			
+			function animate() {
+			
+			    requestAnimFrame( animate );
+			
+			    // just for fun, lets rotate mr rabbit a little
+			    for(var i = 0; i < bunnyCount; i++) {
+			    	bunnies[i].rotation += bunnies[i].rotationSpeed;
+			    }
+				
+			    // render the stage   
+			    renderer.render(stage);
 			}
 
-			m_DOMtra.start();
 
 		});
 
